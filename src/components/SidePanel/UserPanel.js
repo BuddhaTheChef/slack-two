a7a73b00f5738140d05b2e63d0f63f1d2f12a2ed
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-11-29 10:51:16 -0800

    displayed photo URL with current logged in user

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,57 +1,69 @@
-import React, { Component } from 'react';
-import firebase from '../config/firebase';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
-
+import React, { Component } from "react";
+import firebase from "../config/firebase";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 
 class UserPanel extends Component {
-     state = {
-        user: this.props.currentUser
+  state = {
+    user: this.props.currentUser
+  };
+
+  dropdownOptions = () => [
+    {
+      key: "user",
+      text: (
+        <span>
+          Logged in as <strong>{this.state.user.displayName}</strong>
+        </span>
+      ),
+      disabled: true
+    },
+    {
+      key: "avatar",
+      text: <span>Change Avatar</span>
+    },
+    {
+      key: "signout",
+      text: <span onClick={this.handleSignout}>Sign Out</span>
     }
+  ];
 
-    dropdownOptions = () => [
-        {
-            key: 'user',
-            text: <span>Logged in as <strong>{this.state.user.displayName}</strong></span>,
-            disabled: true
-        },
-        {
-            key: 'avatar',
-            text: <span>Change Avatar</span>
-        },
-        {
-            key: 'signout',
-            text: <span onClick={this.handleSignout}>Sign Out</span>
-        }
-    ];
+  handleSignout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => alert("Signed Out!!!!"));
+  };
 
-    handleSignout = () => {
-        firebase.auth().signOut().then(() => alert('Signed Out!!!!'))
-    }
+  render() {
+    const { user } = this.state;
 
-    render() {
-        console.log(this.props.currentUser)
-        return (
-            <Grid style={{ background: '4c3c4c'}}>
-                <Grid.Column>
-                    <Grid.Row style={{ padding: '1.2em', margin: 0}}>
-                        {/* App Header */}
-                        <Header inverted floated="left" as="h2">
-                            <Icon name="code" />
-                            <Header.Content>TechChat</Header.Content>
-                        </Header>
-                    </Grid.Row>
+    return (
+      <Grid style={{ background: "4c3c4c" }}>
+        <Grid.Column>
+          <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
+            {/* App Header */}
+            <Header inverted floated="left" as="h2">
+              <Icon name="code" />
+              <Header.Content>TechChat</Header.Content>
+            </Header>
 
-                     {/* User Dropdown */}
-                     <Header style={{padding: '0.25em'}} as="h4" inverted>
-                        <Dropdown 
-                        trigger={<span>{this.state.user.displayName}</span>} 
-                        options={this.dropdownOptions()} 
-                        />
-                     </Header>
-                </Grid.Column>
-            </Grid>
-        )
-    }
+            {/* User Dropdown */}
+            <Header style={{ padding: "0.25em" }} as="h4" inverted>
+              <Dropdown
+                trigger={
+                  <span>
+                    <Image src={user.photoURL} spaced="right" avatar />
+                    {user.displayName}
+                  </span>
+                }
+                options={this.dropdownOptions()}
+              />
+            </Header>
+          </Grid.Row>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default UserPanel;
