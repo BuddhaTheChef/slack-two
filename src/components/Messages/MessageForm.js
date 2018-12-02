e57e1152624504ccbcfffd55fe9cf10d3027c531
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-02 12:31:22 -0800

    added in modal for uploading images

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { Segment, Input, Button } from 'semantic-ui-react';
 import firebase from '../config/firebase';
+import FileModal from './FileModal';
 
 
 class MessageForm extends Component {
@@ -9,8 +10,13 @@ class MessageForm extends Component {
         channel: this.props.currentChannel,
         user: this.props.currentUser,
         loading: false,
-        errors: []
+        errors: [],
+        modal: false
     }
+
+    openModal = () => {this.setState({modal: true})}
+
+    closeModal = () => {this.setState({modal: false})}
 
     handleChange = (event) => {
         this.setState({[event.target.name]: event.target.value});
@@ -52,7 +58,7 @@ class MessageForm extends Component {
     }
 
     render() {
-        const { errors, message, loading } = this.state;
+        const { errors, message, loading, modal } = this.state;
         return (
             <Segment className='message_form'>
                 <Input
@@ -77,10 +83,16 @@ class MessageForm extends Component {
                     />
                     <Button
                     color='teal'
+                    onClick={this.openModal}
                     content='Upload Media'
                     labelPosition='right'
                     icon='cloud upload'
                     />
+
+                    <FileModal
+                    modal={modal}
+                    closeModal={this.closeModal}
+                      />
                 </Button.Group>
             </Segment>
         )
