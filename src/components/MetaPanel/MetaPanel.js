846249b4c8f6470f8df83fef14f94156cac0b58f
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-07 15:59:46 -0800

    dynamically added details to sidebar about channel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Segment, Accordion, Header, Icon } from 'semantic-ui-react';
+import { Segment, Accordion, Header, Icon, Image } from 'semantic-ui-react';
 
 class MetaPanel extends Component {
     state = {
+        channel: this.props.currentChannel,
         privateChannel: this.props.isPrivateChannel,
         activeIndex: 0
     }
@@ -15,13 +16,13 @@ class MetaPanel extends Component {
     }
 
     render() {
-        const { activeIndex, privateChannel } = this.state;
+        const { activeIndex, privateChannel, channel } = this.state;
 
         if(privateChannel) return null;
 
         return (
-            <Segment> 
-                <Header as='h3' attached='top'>About #Channel</Header>
+            <Segment loading={!channel}> 
+                <Header as='h3' attached='top'>About #{channel && channel.name}</Header>
                 <Accordion styled attache="true">
                     <Accordion.Title
                         active={activeIndex === 0}
@@ -33,7 +34,7 @@ class MetaPanel extends Component {
                        Channel Details
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 0}>
-                        details
+                        {channel && channel.details}
                     </Accordion.Content>
 
                         <Accordion.Title
@@ -58,7 +59,10 @@ class MetaPanel extends Component {
                        Created By
                     </Accordion.Title>
                     <Accordion.Content active={activeIndex === 2}>
-                        creator
+                      <Header as="h3">
+                        <Image circular src={channel && channel.createdBy.avatar} />
+                        {channel && channel.createdBy.name}
+                     </Header>
                     </Accordion.Content>
                 </Accordion>
             </Segment>
