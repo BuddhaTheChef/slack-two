214062dd151f62cd5e7a1c6993c4e1016c31da24
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-07 14:47:14 -0800

    able to favorite and unfavorite channels to display

--- src/components/SidePanel/Favorited.js
+++ src/components/SidePanel/Favorited.js
@@ -1,12 +1,43 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import firebase from '../config/firebase';
 import { setCurrentChannel, setPrivateChannel } from '../../actions';
 import { Menu, Icon} from 'semantic-ui-react';
 
 class Favorited extends Component {
     state = {
+        user: this.props.currentUser,
+        usersRef: firebase.database().ref('users'),
         activeChannel: '',
         favoritedChannels: []
+    }
+
+    componentDidMount() {
+        if(this.state.user) {
+            this.addListeners(this.state.user.uid);
+        }
+    }
+
+    addListeners = (userId) => {
+        this.state.usersRef
+          .child(userId)
+          .child('favorited')
+          .on('child_added', snap => {
+              const favoritedChannel = { id: snap.key, ...snap.val()};
+              this.setState({
+                  favoritedChannels:[...this.state.favoritedChannels, favoritedChannel]
+              })
+          })
+       this.state.usersRef
+          .child(userId)
+          .child('favorited')  
+          .on('child_removed', snap => {
+            const channelUnfavorited = { id: snap.key, ...snap.val()};
+            const filteredChannels = this.state.favoritedChannels.filter(channel => {
+                return channel.id !== channelUnfavorited.id;
+            })
+            this.setState({ favoritedChannels: filteredChannels })
+          })
     }
 
     setActiveChannel = (channel) => {
