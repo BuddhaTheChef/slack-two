5a77919110f65c9f1a6900d9a2408933f9539d4b
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-09 14:37:10 -0800

    added in modal for changing and previewing images

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import firebase from "../config/firebase";
-import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
+import { Grid, Header, Icon, Dropdown, Image, Modal, Input, Button } from "semantic-ui-react";
 
 class UserPanel extends Component {
   state = {
-    user: this.props.currentUser
+    user: this.props.currentUser,
+    modal: false
   };
+
+  openModal = () => this.setState({ modal: true });
+
+  closeModal = () => this.setState({ modal: false });
 
   dropdownOptions = () => [
     {
@@ -19,7 +24,7 @@ class UserPanel extends Component {
     },
     {
       key: "avatar",
-      text: <span>Change Avatar</span>
+      text: <span onClick={this.openModal}>Change Avatar</span>
     },
     {
       key: "signout",
@@ -35,7 +40,7 @@ class UserPanel extends Component {
   };
 
   render() {
-    const { user } = this.state;
+    const { user, modal } = this.state;
     const { primaryColor } =this.props;
 
     return (
@@ -61,6 +66,40 @@ class UserPanel extends Component {
               />
             </Header>
           </Grid.Row>
+
+          {/* Change Profile Pic Modal */}
+          <Modal basic open={modal} onClose={this.closeModal}>
+            <Modal.Header>Change Profile Image</Modal.Header>
+            <Modal.Content>
+              <Input
+                fluid
+                type="file"
+                label="New Pic"
+                name="previewImage"
+               />
+               <Grid centered stackable columns={2}>
+                  <Grid.Row centered>
+                    <Grid.Column className="ui centered aligned grid">
+                      {/* Image Preview */}
+                    </Grid.Column>
+                    <Grid.Column>
+                      {/* Cropped Image Preview */}
+                    </Grid.Column>
+                  </Grid.Row>
+               </Grid>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button color='blue' inverted>
+                <Icon name='save' /> Change Avatar
+              </Button>
+              <Button color='purple' inverted>
+                <Icon name='image' /> Preview
+              </Button>
+              <Button color='red' inverted onClick={this.closeModal}>
+                <Icon name='remove' /> Cancel
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </Grid.Column>
       </Grid>
     );
