350f16794be15ec3e4e9eec1286a0add00280168
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-12-11 14:44:21 -0800

    finished user typing function able to see throughout users

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -22,7 +22,10 @@ class Messages extends Component {
         allUsers: '',
         searchTerm: '',
         searchLoad: false,
-        searchResults: []
+        searchResults: [],
+        typingRef: firebase.database().ref('typing'),
+        typingUsers: [],
+        connectedRef: firebase.database().ref('.info/connected')
     }
 
     componentDidMount() {
@@ -35,6 +38,40 @@ class Messages extends Component {
 
     addListeners = (channelId) => {
         this.addMessageListener(channelId);
+        this.addTypingListeners(channelId);
+    }
+
+    addTypingListeners = (channelId) => {
+        let typingUsers = [];
+        this.state.typingRef.child(channelId).on('child_added', snap => {
+            if(snap.key !== this.state.user.uid) {
+                typingUsers = typingUsers.concat({
+                    id: snap.key,
+                    name: snap.val()
+                })
+                this.setState({ typingUsers })
+            }
+        })
+        this.state.typingRef.child(channelId).on('child_removed', snap => {
+            const index = typingUsers.findIndex(user => user.id === snap.key);
+            if(index !== -1) {
+                typingUsers = typingUsers.filter(user => user.id !== snap.key);
+                this.setState({ typingUsers });
+            }
+        })
+        this.state.connectedRef.on('value', snap => {
+            if(snap.val() === true) {
+                this.state.typingRef
+                .child(channelId)
+                .child(this.state.user.uid)
+                .onDisconnect()
+                .remove(err => {
+                    if(err !== null) {
+                        console.error(err);
+                    }
+                })
+            }
+        })
     }
 
     addMessageListener = (channelId) => {
@@ -57,7 +94,7 @@ class Messages extends Component {
         .child('favorited')
         .once('value')
         .then(data => {
-            if(data.val !== null) {
+            if(data.val() !== null) {
                 const channelIds = Object.keys(data.val());
                 const prevFavorited = channelIds.includes(channelId);
                 this.setState({isChannelFavorited: prevFavorited});
@@ -166,8 +203,16 @@ class Messages extends Component {
         return channel ? `${this.state.privateChannel ? '@' : '#'}${channel.name}` : '';
     };
 
+    displayTypingUsers = (users) => (
+        users.length > 0 && users.map(user => (
+            <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.2em'}} key={user.id}>
+            <span className="user_typing">{user.name} is typing</span><Typing />
+            </div>
+        ))
+    )
+
     render() {
-        const { messagesRef, messages, channel, user, allUsers, searchTerm, searchResults, searchLoad, privateChannel, isChannelFavorited } = this.state;
+        const { messagesRef, messages, channel, user, allUsers, searchTerm, searchResults, searchLoad, privateChannel, isChannelFavorited, typingUsers } = this.state;
         return (
             <React.Fragment>
                 <MessagesHeader
@@ -183,9 +228,7 @@ class Messages extends Component {
                 <Segment>
                     <Comment.Group className='messages'>
                         {searchTerm ? this.displayMessages(searchResults): this.displayMessages(messages)}
-                        <div style={{display: 'flex', alignItems: 'center'}}>
-                        <span className="user_typing">User is typing</span><Typing />
-                        </div>
+                        {this.displayTypingUsers(typingUsers)}
                     </Comment.Group>
                 </Segment>
                 
